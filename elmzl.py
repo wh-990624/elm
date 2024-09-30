90330029e2e8d73822f5e54ef9cd4757b169573f
promisewu <promisewu@tencent.com>
2024-09-30 09:53:32 +0800

    1

--- elmzl.py
+++ elmzl.py
@@ -1,145 +1,50 @@
-import hashlib
+#
+# 变量：elmck: 必填，账号cookie
+# cron: 15 2 * * *
+#
+
+# const $ = new Env('邀请助力');
+
 import json
 import os
-import time
-
 import requests
 from urllib.parse import quote
 import datetime
 
 host = 'https://acs.m.goofish.com'
 
-zlck = os.environ.get('elmzlck')
+elmzlck = os.environ.get('elmzlck')
+zlck = os.environ.get('elmck')
 
-ck = ""
+ck = ''
 
-
-def get_cookie_map(cookie_string):
-    """
-    解析为cookie_map
-    """
-    cookie_pairs = cookie_string.split(";")
-    cookie_map = {}
-    for pair in cookie_pairs:
-        if "=" in pair:
-            name, value = pair.split("=", 1)
-            cookie_map[name.strip()] = value.strip()
-        else:
-            continue
-    return cookie_map
-
-
-def map_to_cookie_string(cookie_map):
-    """
-    将cookie_map还原为cookie
-    """
-    cookie_string = ""
-    for key, value in cookie_map.items():
-        cookie_string += f"{key}={value};"
-    return cookie_string.strip()
-
-
-def hbh5tk(tk_cookie, enc_cookie, cookie_str):
-    """
-    合并带_m_h5_tk
-    """
-    txt = cookie_str.replace(" ", "")
-    if txt[-1] != ';':
-        txt += ';'
-    cookie_parts = txt.split(';')[:-1]
-    updated = False
-    for i, part in enumerate(cookie_parts):
-        key_value = part.split('=')
-        if key_value[0].strip() in ["_m_h5_tk", " _m_h5_tk"]:
-            cookie_parts[i] = tk_cookie
-            updated = True
-        elif key_value[0].strip() in ["_m_h5_tk_enc", " _m_h5_tk_enc"]:
-            cookie_parts[i] = enc_cookie
-            updated = True
-
-    if updated:
-        return ';'.join(cookie_parts) + ';'
-    else:
-        return txt + tk_cookie + ';' + enc_cookie + ';'
-
-
-def tq(cookie_string):
-    """
-    获取_m_h5_tk
-    """
-    if not cookie_string:
-        return '-1'
-    cookie_pairs = cookie_string.split(';')
-    for pair in cookie_pairs:
-        key_value = pair.split('=')
-        if key_value[0].strip() in ["_m_h5_tk", " _m_h5_tk"]:
-            return key_value[1]
-    return '-1'
-
-
-def tq1(txt):
-    """
-    拆分cookie
-    """
+def tq(txt):
+    if not isinstance(txt, str):
+        print("❎输入不是字符串")
+        return {}
     try:
         txt = txt.replace(" ", "")
-        txt = txt.replace("chushi;", "")
-        if txt[-1] != ';':
-            txt += ';'
-        pairs = txt.split(";")[:-1]
+        pairs = txt.split(";")
         ck_json = {}
         for pair in pairs:
-            key, value = pair.split("=", 1)
-            ck_json[key] = value
+            if '=' in pair:
+                key, value = pair.split("=", 1)  # 只分割第一个"="
+                if key and value:  # 确保键和值都不为空
+                    ck_json[key] = value
         return ck_json
     except Exception as e:
         print(f'❎Cookie解析错误: {e}')
         return {}
 
-
-def md5(text):
-    """
-    md5加密
-    """
-    hash_md5 = hashlib.md5()
-    hash_md5.update(text.encode())
-    return hash_md5.hexdigest()
-
-
-def check_cookie(cookie):
-    url = "https://waimai-guide.ele.me/h5/mtop.alsc.personal.queryminecenter/1.0/?jsv=2.6.2&appKey=12574478"
-    headers = {
-        "Cookie": cookie,
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36"
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            cookie_jar = response.cookies
-            token = cookie_jar.get('_m_h5_tk', '')
-            token_cookie = "_m_h5_tk=" + token
-            enc_token = cookie_jar.get('_m_h5_tk_enc', '')
-            enc_token_cookie = "_m_h5_tk_enc=" + enc_token
-            cookie = hbh5tk(token_cookie, enc_token_cookie, cookie)
-            return cookie
-        else:
-            return None
-    except Exception as e:
-        print("解析ck错误")
-        return None
-
-
 class LYB:
     def __init__(self, cki):
-        self.cookie = cki
-        self.ck1 = tq1(cki)
+        self.ck1 = tq(cki)
         self.uid = self.ck1.get("unb")
         self.sid = self.ck1.get("cookie2")
         self.name = self.uid
 
     def xsign(self, api, data, wua, v):
-        url = "http://v1.message.mf0a.cn/commonApi/getelmsign?token=dahuang419478052"
+        url = "http://192.168.31.48:9999/api/getXSign"
         body = {
             "data": data,
             "api": api,
@@ -151,9 +56,9 @@ class LYB:
             "wua": wua,
             'ttid': '1551089129819@eleme_android_10.14.3',
             "v": v
-        }
+}
 
-        max_retries = 5
+        max_retries = 1
         retries = 0
         while retries < max_retries:
             try:
@@ -168,7 +73,73 @@ class LYB:
             retries += 1
             print(f"❎重试次数: {retries}")
             if retries >= max_retries:
-                print("❎尝试次数上限,哦豁，签名通道不可用！")
+                print("❎重试次数上限,尝试使用备用通道1")
+                return self.xsign1(api, data, wua, v)
+
+    def xsign1(self, api, data, wua, v):
+        url = "http://mzkj666.cn:9324/encrypt"
+        body = {
+            "data": data,
+            "api": api,
+            "pageId": '',
+            "uid": self.uid,
+            'sid': self.sid,
+            "deviceId": '',
+            "utdid": '',
+            "wua": wua,
+            'ttid': '1551089129819@eleme_android_10.14.3',
+            "v": v
+        }
+
+        max_retries = 1
+        retries = 0
+        while retries < max_retries:
+            try:
+                r = requests.post(url, json=body, timeout=9)
+                return r.json()
+            except requests.exceptions.HTTPError as e:
+                print(f'❎请求签名服务器失败: {e}')
+            except requests.exceptions.Timeout:
+                print("❎签名接口请求超时")
+            except requests.exceptions.RequestException as e:
+                print(f'❎请求签名服务器错误: {e}')
+            retries += 1
+            print(f"❎重试次数: {retries}")
+            if retries >= max_retries:
+                print("❎通道1尝试次数上限,尝试使用备用通道2")
+                return self.xsign2(api, data, wua, v)
+
+    def xsign2(self, api, data, wua, v):
+        url = "http://192.168.124.104:9999/api/get"
+        body = {
+            "data": data,
+            "api": api,
+            "pageId": '',
+            "uid": self.uid,
+            'sid': self.sid,
+            "deviceId": '',
+            "utdid": '',
+            "wua": wua,
+            'ttid': '1551089129819@eleme_android_10.14.3',
+            "v": v
+        }
+
+        max_retries = 1
+        retries = 0
+        while retries < max_retries:
+            try:
+                r = requests.post(url, json=body, timeout=9)
+                return r.json()
+            except requests.exceptions.HTTPError as e:
+                print(f'❎请求签名服务器失败: {e}')
+            except requests.exceptions.Timeout:
+                print("❎签名接口请求超时")
+            except requests.exceptions.RequestException as e:
+                print(f'❎请求签名服务器错误: {e}')
+            retries += 1
+            print(f"❎重试次数: {retries}")
+            if retries >= max_retries:
+                print("❎通道2尝试次数上限,哦豁，签名通道都不可用！")
                 return None
 
     def req(self, api, data, wua='False', v="1.0"):
@@ -218,45 +189,6 @@ class LYB:
             print(f'❎请求接口失败: {e}')
             return None
 
-    def req1(self, api, data, v="1.0"):
-        try:
-            cookie = check_cookie(self.cookie)
-            timestamp = int(time.time() * 1000)
-            data_str = json.dumps(data)
-            token = tq(cookie)
-            token_part = token.split("_")[0]
-
-            sign_str = f"{token_part}&{timestamp}&12574478&{data_str}"
-            sign = md5(sign_str)
-            url = f"https://guide-acs.m.taobao.com/h5/{api}/{v}/?jsv=2.6.1&appKey=12574478&t={timestamp}&sign={sign}&api={api}&v={v}&type=originaljson&dataType=json"
-            headers = {
-                "authority": "shopping.ele.me",
-                "accept": "application/json",
-                "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-                "cache-control": "no-cache",
-                "content-type": "application/x-www-form-urlencoded",
-                "cookie": cookie,
-                "user-agent": "Mozilla/5.0 (Linux; Android 8.0.0; SM-G955U Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36"
-            }
-            max_retries = 5
-            retries = 0
-            while retries < max_retries:
-                try:
-                    r = requests.post(url, headers=headers, data={'data': data_str})
-                    return r
-                except requests.exceptions.Timeout:
-                    print("❎接口请求超时")
-                except requests.exceptions.RequestException as e:
-                    print(f"❎请求异常: {e}")
-                retries += 1
-                print(f"❎重试次数: {retries}")
-                if retries >= max_retries:
-                    print("❎重试次数上限")
-                    return None
-        except Exception as e:
-            print(f'❎请求接口失败: {e}')
-            return None
-
     def yqm(self):
         api = 'mtop.ele.biz.growth.task.core.querytask'
         data = json.dumps({"missionCollectionId": "839",
@@ -266,6 +198,7 @@ class LYB:
             res = self.req(api, data, 'False' "1.0")
             if res.json()["ret"][0] == "SUCCESS::接口调用成功":
                 for y in res.json()['data']['mlist']:
+                    print(y['name'])
                     if y['name'] == "邀请好友助力":
                         actId = y['actionConfig']['ext']['actId']
                         ShareId = y['actionConfig']['ext']['shareId']
@@ -283,9 +216,10 @@ class LYB:
 
     def share(self, actid1, shareId1):
         api = 'mtop.koubei.interactioncenter.share.common.triggershare'
-        data = {"actId": actid1, "shareId": shareId1, "bizScene": "DEFAULT", "requestId": "1719848804784"}
+        data = json.dumps(
+            {"actId": actid1, "shareId": shareId1, "bizScene": "DEFAULT", "requestId": "1719848804784"})
         try:
-            res = self.req1(api, data, "1.0")
+            res = self.req(api, data, 'False' "1.0")
             if res is None:
                 return None
             if res.json()["ret"][0] == "SUCCESS::调用成功":
@@ -318,11 +252,11 @@ class LYB:
 
     def prize(self):
         api1 = 'mtop.ele.biz.growth.task.core.querytask'
-        data1 = {"missionCollectionId": "839",
-                 "locationInfos": "[\"{\\\"lng\\\":\\\"105.75325090438128\\\",\\\"lat\\\":\\\"30.597472842782736\\\"}\"]",
-                 "bizScene": "game_center", "accountPlan": "HAVANA_COMMON"}
+        data1 = json.dumps({"missionCollectionId": "839",
+                            "locationInfos": "[\"{\\\"lng\\\":\\\"105.75325090438128\\\",\\\"lat\\\":\\\"30.597472842782736\\\"}\"]",
+                            "bizScene": "game_center", "accountPlan": "HAVANA_COMMON"})
         try:
-            res1 = self.req(api1, data1, 'False', "1.0")
+            res1 = self.req(api1, data1, 'False' "1.0")
             if res1 is None:
                 return None
             if res1.json()["ret"][0] == "SUCCESS::接口调用成功":
@@ -331,13 +265,13 @@ class LYB:
                         for o in y['missionStageDTOS']:
                             if o['rewardStatus'] == "TODO" and o['status'] == "FINISH":
                                 api = 'mtop.ele.biz.growth.task.core.receiveprize'
-                                data2 = {
+                                data2 = json.dumps({
                                     "missionCollectionId": "839",
                                     "missionId": "20544001",
                                     "count": o['stageCount']
-                                }
+                                })
                                 try:
-                                    res = self.req1(api, data2, "1.0")
+                                    res = self.req(api, data2, 'False' "1.0")
                                     if res is None:
                                         continue
                                     data = res.json()["data"]
@@ -395,31 +329,27 @@ if __name__ == '__main__':
     if cookie == "":
         print("❎本地变量为空，请设置其中一个变量后再运行")
         exit(-1)
-
+    # cookie_str = ck['elmck']
     cookies = cookie.split("&")
 
-    zlck_list = zlck.split("&")
-    print(f"获取到 {len(zlck_list)} 个被助力账号")
+    elmzlck_list = elmzlck.split("&")
+    print(f"获取到 {len(elmzlck_list)} 个被助力账号")
 
     dzl_num = 0
-    for zlck in zlck_list:
+    for elmzlck in elmzlck_list:
         dzl_num += 1
-        lyb = LYB(zlck)
-        nm = get_ck_usid(zlck)
+        lyb = LYB(elmzlck)
         actid, shareId = lyb.yqm()
         print(actid)
         if actid is None or shareId is None:
             print("❎获取助力id失败")
         else:
-            print(f"======被助力账号[{dzl_num}]--[{nm}]获取邀请码成功,开始助力======")
+            print(f"======被助力账号{dzl_num}获取邀请码成功,开始助力======")
             for i, ck in enumerate(cookies):
-                if ck == zlck:
-                    continue
-                ck = ck.replace("chushi;", "")
                 usid = get_ck_usid(ck)
                 zlcs = data.get(f"{usid}", 0)
                 if zlcs < 3:
-                    print(f"======被助力账号[{dzl_num}]--[{nm}]-开始第{i + 1}/{len(cookies)}个账号助力======")
+                    print(f"======被助力账号{dzl_num}-开始第{i + 1}/{len(cookies)}个账号助力======")
                     a = LYB(ck).share(actid, shareId)
                     if a == 'SX':
                         break
@@ -440,6 +370,6 @@ if __name__ == '__main__':
                         continue
                 else:
                     continue
-        print(f"======被助力账号{nm}-领取奖励======")
+        print(f"======被助力账号{dzl_num}-领取奖励======")
         lyb.prize()
-        print(f"======被助力账号{nm}-助力结束======\n\n")
+        print(f"======被助力账号{dzl_num}-助力结束======\n\n")
